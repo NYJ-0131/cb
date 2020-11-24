31c07161994d145da4d6a6f576df14d30737125c
admin <a>
2020-11-24 17:37:33 +0800

    1

---
+++ src/main/java/com/example/cb/CbApplication.java
@@ -0,0 +1,16 @@
+package com.example.cb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CbApplication.class, args);
+		System.out.println("服务器已启动");
+	}
+
+}
